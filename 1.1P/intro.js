3cc6a2b5371e3e345ae632254ba6bd061684ed57
MofarehAlmakhalas <aibusinesshelperai@gmail.com>
2026-07-06 19:32:24 +1000

    Complete SIT725 Task 1.1P

---
+++ 1.1P/intro.js
@@ -0,0 +1,8 @@
+let studentName = "Mofareh";
+let studentID = "221172728";
+let unit = "SIT725 Applied Software Engineering";
+
+console.log("Student Name: " + studentName);
+console.log("Student ID: " + studentID);
+console.log("Unit: " + unit);
+console.log("Welcome to Week 1 programming activity.");
